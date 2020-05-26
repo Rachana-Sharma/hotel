bd939c2eba5857b1a9c639da34c5706f74e8ec39
Rachana-Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-05-26 16:03:40 +0530

    Update HotelApplication.java

--- Hotel/src/main/java/com/hotel/HotelApplication.java
+++ Hotel/src/main/java/com/hotel/HotelApplication.java
@@ -1,11 +1,15 @@
 package com.hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Rachana Sharma
+ * Main Class : HotelApplication
+ */
 @SpringBootApplication
-public class HotelApplication 
-{
-public static void main(String[] args) 
-{
-SpringApplication.run(HotelApplication.class, args);
+public class HotelApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+	}
 }
-}
+
