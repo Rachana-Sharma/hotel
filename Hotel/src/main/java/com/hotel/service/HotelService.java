c55573d3a6fdde76da9b05ea213df77b76962178
Rachana-Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-05-26 16:09:23 +0530

    Update HotelService.java

--- Hotel/src/main/java/com/hotel/service/HotelService.java
+++ Hotel/src/main/java/com/hotel/service/HotelService.java
@@ -7,30 +7,43 @@ import org.springframework.stereotype.Service;
 import com.hotel.model.Hotel;
 import com.hotel.repository.HotelRepository;
 
-@Service
-public class HotelService 
-{
-@Autowired
-HotelRepository hotelRepository;
-
-public List<Hotel> getAllHotel() 
-{
-List<Hotel> hotelList = new ArrayList<Hotel>();
-hotelRepository.findAll().forEach(hotel -> hotelList.add(hotel));
-return hotelList;
-}
-
-public Hotel getHotelById(int id) 
-{
-return hotelRepository.findById(id).get();
-}
-public void saveHotel(Hotel hotel) 
-{
-hotelRepository.save(hotel);
-}
-
-public void deleteHotel(int id) 
-{
-hotelRepository.deleteById(id);
-}
+/**
+ * @author Rachana Sharma
+ * Service Class: HotelService
+ */
+	@Service
+	public class HotelService {
+	@Autowired
+	HotelRepository hotelRepository;
+	
+	/**
+	 * @return hotelList
+	 * to add new hotel to the ArrayList
+	 */
+	public List<Hotel> getAllHotel() {
+		List<Hotel> hotelList = new ArrayList<Hotel>();
+		hotelRepository.findAll().forEach(hotel -> hotelList.add(hotel));
+		return hotelList;
+	}
+	/**
+	 * @param id
+	 * @return saved values from repository against the given id 
+	 */
+	public Hotel getHotelById(int id) {
+		return hotelRepository.findById(id).get();
+	}
+	/**
+	 * @param hotel
+	 * to save into hotel repository
+	 */
+	public void saveHotel(Hotel hotel) {
+		hotelRepository.save(hotel);
+	}
+	/**
+	 * @param id
+	 * to delete from repository against given id
+	 */
+	public void deleteHotel(int id) {
+		hotelRepository.deleteById(id);
+	}
 }
