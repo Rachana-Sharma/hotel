ea4009897e1abc4e02f5331af9b120f1b710f907
Rachana-Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-05-26 16:06:05 +0530

    Update HotelController.java

--- Hotel/src/main/java/com/hotel/controller/HotelController.java
+++ Hotel/src/main/java/com/hotel/controller/HotelController.java
@@ -16,34 +16,41 @@ import com.hotel.service.HotelService;
  * Hotel Controller
  */
 @RestController
-public class HotelController 
-{
+public class HotelController {
 
-@Autowired
-HotelService hotelService;
+	@Autowired
+	HotelService hotelService;
  
-@GetMapping("/hotel/get")
-private List<Hotel> getAllHotel() 
-{
-return hotelService.getAllHotel();
-}
-
-@GetMapping("/hotel/{id}")
-private Hotel getHotel(@PathVariable("id") int id) 
-{
-return hotelService.getHotelById(id);
-}
-
-@DeleteMapping("/hotel/delete/{id}")
-private void deleteHotels(@PathVariable("id") int id) 
-{
-hotelService.deleteHotel(id);
-}
-
-@PostMapping("/hotel/save")
-private int saveHotel(@RequestBody Hotel hotel) 
-{
-hotelService.saveHotel(hotel);
-return hotel.getId();
-}
+	/**
+	 * @return List of all hotel 
+	 */
+	@GetMapping("/hotel/get")
+	private List<Hotel> getAllHotel() {
+		return hotelService.getAllHotel();
+	}
+	/**
+	 * @param id
+	 * @return saved details against given id
+	 */
+	@GetMapping("/hotel/{id}")
+	private Hotel getHotel(@PathVariable("id") int id) {
+		return hotelService.getHotelById(id);
+	}
+	/**
+	 * @param id
+	 * delete from repository against given id
+	 */
+	@DeleteMapping("/hotel/delete/{id}")
+	private void deleteHotels(@PathVariable("id") int id) {
+		hotelService.deleteHotel(id);
+	}
+	/**
+	 * @param hotel
+	 * @return id of hotel that has been saved to repository
+	 */
+	@PostMapping("/hotel/save")
+	private int saveHotel(@RequestBody Hotel hotel) {
+		hotelService.saveHotel(hotel);
+		return hotel.getId();
+	}
 }
